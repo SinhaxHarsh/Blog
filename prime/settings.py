c5b9e536415f1552ee9b67ddba4ffb1e722e8c46
Sinha <132650246+SinhaxHarsh@users.noreply.github.com>
2025-02-18 11:10:29 +0530

    Update settings.py

--- prime/settings.py
+++ prime/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-80qp6d#%rn&2=sdgf4-$06vy6ifmwiz#*qrxswjfmr2%44ni1f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
